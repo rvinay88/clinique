b8cf1e43a2b539014f52014d95c42fcffed10754
Vinay Raghu <rvinay88@gmail.com>
2014-12-03 16:02:19 -0500

    Add room class and Room DAL class

---
+++ clinique/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clinique
+{
+    class Room
+    {
+        public int RoomID { get; private set; }
+        public string RoomName { get; private set; }
+
+        public Room(int roomID, string roomName)
+        {
+            RoomID = roomID;
+            RoomName = roomName;
+        }
+    }
+}
---
+++ clinique/roomDAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clinique
+{
+    class roomDAL
+    {
+        SqlConnection mySqlConnection = null;
+        public roomDAL(string connectionString)
+        {
+            mySqlConnection = new SqlConnection(connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            mySqlConnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            mySqlConnection.Close();
+        }
+
+        public int CreateRoom(Room Room)
+        {
+            int result = 0;
+
+            SqlCommand mySqlCommand = new SqlCommand();
+            mySqlCommand.CommandText = "CreateRoom";
+            mySqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            mySqlCommand.Connection = mySqlConnection;
+            mySqlCommand.Parameters.AddWithValue("@roomID", room.RoomID);
+            mySqlCommand.Parameters.AddWithValue("@roomName", room.RoomName);
+            result = mySqlCommand.ExecuteNonQuery();
+            
+            return result;
+        }
+    }
+}
